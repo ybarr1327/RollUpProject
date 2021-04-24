09dc4ce352ae9e5e505ecac87204ec2b1e722e47
Gabriel Ybarra <ybarr015@cougars.csusm.edu>
2021-04-24 03:48:23 -0700

    made it so there are some checks on the button and that it shows a confirmation or error based on if the class was actually signed up for
    so basically at this point the button will only reload the page if no checkbox is selected. if a checkbox is selected then it is able to go to the signup page but it first saves the data.
    
    there is a check to make sure the user doesn't sign up for a full class, if the sign up is successful, i store the id so that later on the signup page i can show that that class was signed up for. i do the same for the ones that would fail.
    
    on the signup page it shows a confirmation of what classes were and weren't signed up for based on the lists
    
    check the comments on the views.py to get a more verbose explanation

--- RollUp/Pages/views.py
+++ RollUp/Pages/views.py
@@ -43,38 +43,95 @@ def AccountDashPage(request):
     return render(request, "accountDashPage/accountDashPage.html")
 @login_required
 def SchedulePage(request):
-    all_classes = Classes.objects.order_by('date')
-    contextOfClasses = {
+    all_classes = Classes.objects.order_by('date') #get all the entries from the classes table ordered by date
+    #store the context of the classes to pass to the render
+    contextOfClasses = {  
         'classes' : all_classes
     }
 
+    #if the user pressed the sign up button
     if request.method == 'POST' and 'SignUpForClass' in request.POST:
-        user = request.user
-        #get the user data
-
         
-        #get the clicked checkboxes
+        
+        #get the clicked checkboxes, this gets a list of numbers that contain the checkbox id's, 
+        # these ids are set to the value of the class id they are representing
         check_boxes = request.POST.getlist('checkbox')
-        # print(check_boxes)
-        for i in check_boxes:
-            newParticipant = Participants()
-            newParticipant.email = user.email
-            newParticipant.name = user.first_name +" "+ user.last_name
-            newParticipant.class_id = Classes.objects.get(id = i)
-            newParticipant.save()
-        
-        
-        
+        if check_boxes: #if there were objects checked
+            user = request.user # get the user data
             
-        
-        # return user to required page
-        return redirect('/accountDash/schedule/signup/') 
+            # the following are lists to store which classes were able to be signed up for or not
+            sucessfulClassSignups = []
+            failedClassSignups = []
+            
+            # print(check_boxes)
+            
+            #for all the class ids / checkboxes selected
+            for i in check_boxes:
+                #get the class object associated with that class id
+                classToSignUpFor = Classes.objects.get(id=i)
+                
+                # if there is room in that class, sign up
+                if classToSignUpFor.num_signed_up < classToSignUpFor.size:
+                    
+                    #add this class id to the sucessful signup list
+                    sucessfulClassSignups.append(classToSignUpFor.id)
+                    
+                    #create a new participant and store it
+                    newParticipant = Participants()
+                    newParticipant.email = user.email
+                    newParticipant.name = user.first_name +" "+ user.last_name
+                    newParticipant.class_id = classToSignUpFor
+                    newParticipant.save()
+                else: # if the class is full, then add it to the fail list
+                    failedClassSignups.append(classToSignUpFor.id)
+            
+            # print(sucessfulClassSignups)
+            # print(failedClassSignups)
 
+            #store the sucess and fail lists in a tuple
+            signupconfimation = (sucessfulClassSignups,failedClassSignups)
+            
+            #store the signup confimation tuple in a session
+            #NOTE: a session is like a hash table / dictionary that stores data on a string key wihtin the database
+            #NOTE: this is mainly useful for passing in between different views / different urls 
+            request.session['signupdata'] = signupconfimation
+
+            
+
+            # return user to required page which is the signup page
+            return redirect('signup')
+
+
+
+        
+    # render the page with the html and the context of classes to show all the available classes
     return render(request, "accountDashPage/schedulePage.html", contextOfClasses)
 
 @login_required
 def SignupClassPage(request):
-    return render(request, "accountDashPage/signupclassPage.html")
+    classes_signed_up_for = request.session['signupdata'] # pull the signup data from the session database/table
+    del request.session['signupdata'] #delete the key/item because it is no longer needed to be stored since we just stored it in a variable
+    
+    #the following code prints the session items currently stored
+    # for key, value in request.session.items():
+    #     print('{} => {}'.format(key, value))
+
+    sucessfulSignups = [] #this is a list that will store the actual class objects of the classes that were sucessfull signed up for
+    failedSignups = [] # this is a list that will store the actual class objects of the classes that were nor able to be signed up for
+
+    for a in classes_signed_up_for[0]: # for all the sucessful ones, add their objects to the list
+        sucessfulSignups.append(Classes.objects.get(id=a))
+    for b in classes_signed_up_for[1]: # for all the failed ones, add their objescts to the list
+        failedSignups.append(Classes.objects.get(id=b))
+
+
+    #strore the two lists as the context dictionary
+    context = { 
+        'succeed' : sucessfulSignups,
+        'failed' : failedSignups
+    }
+    #render the view with the context
+    return render(request, "accountDashPage/signupclassPage.html", context)
 
 @login_required
 def CovidReportPage(request):
